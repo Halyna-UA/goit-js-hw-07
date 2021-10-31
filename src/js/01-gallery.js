ba060e4f1fe2ba5e40012aa193c5edc83f7b7150
Halyna-UA <81983959+Halyna-UA@users.noreply.github.com>
2021-10-31 19:48:02 +0200

    gallery_01
    Не відкриває зображень при активному event.preventDefault();  і не закриває при його відсутності

---
+++ src/js/01-gallery.js
@@ -0,0 +1,41 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+const galleryContainer = document.querySelector('.gallery');
+
+const listGalleryItem = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<div class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-src="${original}"
+          alt="${description}"
+        />
+      </a>
+    </div>`;
+  })
+  .join('');
+
+galleryContainer.insertAdjacentHTML('afterbegin', listGalleryItem);
+
+galleryContainer.addEventListener("click", selectImg);
+
+function selectImg(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "img") {
+    return;
+  }
+  
+  let selectedImg = event.target.getAttribute("data-source");
+
+// import * as basicLightbox from 'basiclightbox'
+
+const instance = basicLightbox.create(`
+    <img data-src="${selectedImg}" width="800" height="600">
+`)
+
+instance.show();
+}
+ console.log(galleryItems);
